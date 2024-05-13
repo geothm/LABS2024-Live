82ba88097e9ad033710f5b7442b3f3ae1fb6676b
gpapp <georgia.papp@irian.ro>
2024-05-13 16:02:59 +0300

    [lab1][ex2]

---
+++ 02-SpringBootPizza/src/main/java/ro/irian/labs2024/Labs2024Application.java
@@ -0,0 +1,13 @@
+package ro.irian.labs2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labs2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labs2024Application.class, args);
+	}
+
+}

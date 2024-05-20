1bf745d8e2c9e43ac2a85e0bb8931a52217a0d0d
gpapp <georgia.papp@irian.ro>
2024-05-20 15:16:48 +0300

    [lab2][ex2] api module

---
+++ 06-MavenModuleOptDep/pizza-domain/src/main/java/ro/irian/labs2024/pizza/domain/PizzaService.java
@@ -0,0 +1,13 @@
+package ro.irian.labs2024.pizza.domain;
+
+import ro.irian.labs2024.pizza.domain.Pizza;
+
+import java.util.List;
+
+public interface PizzaService {
+    List<Pizza> getAllPizzas();
+
+    Pizza getPizzaById(Long id);
+
+    List<Pizza> getPizzaByNameContains(String nameContainsString);
+}
